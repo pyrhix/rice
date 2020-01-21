f802aa3176e9458d0041ff8f61ab99b17ea163cc
Robert <49508689+pyrhix@users.noreply.github.com>
2020-01-20 19:01:04 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 cd ~/
 sudo pacman -S xorg-server xorg-xinit
-sudo pacman -S i3-gaps i3status rxvt-unicode dmenu noto-fonts alsa-utils networkmanager
-sudo systemctl enable NetworkManager
+sudo pacman -S i3-gaps i3status rxvt-unicode dmenu alsa-utils networkmanager
 sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
 sudo pacman -S feh compton ranger w3m firefox youtube-dl neofetch scrot discord syncthing mpd ncmpcpp newsboat htop mpv bluez bluez-utils pulseaudio pavucontrol blueman cmus
 
+cd ~/rice
+mv .xinitrc ~/
+mv .Xresources ~/
+mv .config ~/
+mv .newsboat ~/
+
+
 #yay installation
 git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+#cd yay
+#makepkg -si
 
-cd ~/
-yay -S tamzen-font
-yay -S spotify-adblock-git
+#cd ~/
+#yay -S tamzen-font
+#yay -S spotify-adblock-git
 
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth.service
