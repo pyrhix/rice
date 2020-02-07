c51c96a532d0cd2e25c20e2aa79a6d9bef72bcd7
Robert <49508689+pyrhix@users.noreply.github.com>
2020-02-07 03:14:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ sudo pacman -S xorg-server xorg-xinit
 sudo pacman -S i3-gaps i3status rxvt-unicode dmenu alsa-utils networkmanager
 sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
 sudo pacman -S feh compton ranger w3m youtube-dl neofetch scrot discord syncthing mpd ncmpcpp newsboat htop mpv
-sudo pacman -S bluez bluez-utils pulseaudio pavucontrol blueman pulseaudio-bluetooth
+sudo pacman -S bluez bluez-utils pulseaudio pavucontrol blueman pulseaudio-bluetooth pulseaudio-alsa
 
 cd ~/rice
 mv .xinitrc ~/
